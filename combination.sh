5998ea8f96376d230a956b3ec2511284961a601d
prem-12345 <premkpatil1997@gmail.com>
2021-06-06 11:19:49 +0530

    coin_flip_display_headwin_or_tailwin

--- combination.sh
+++ combination.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 #"FLIP COIN COMBINATION PROBLEM"
+
+#UC_1:COIN FLIP TO DISPLAY HEAD OR TAIL
+
+X=$((RANDOM%2))
+
+if [[ $x -eq 0 ]]
+then
+	echo "Heads win"
+else
+	echo "Tails win"
+fi
