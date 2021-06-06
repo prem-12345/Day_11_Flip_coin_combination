00ea27e020f7ae1789a08806235bbf242178cf60
prem-12345 <premkpatil1997@gmail.com>
2021-06-06 13:04:08 +0530

    singlet_Head_and_Tail

--- combination.sh
+++ combination.sh
@@ -12,3 +12,26 @@ then
 else
 	echo "Tails win"
 fi
+
+#UC_2:COIN FLIP & STORE THE SINGLET COMBINATION IN DICTIONARY & FIND PERCENTAGE
+
+declare -A singlet
+
+singleHead=0
+singleTail=0
+
+for ((i=0;i<50;i++))
+do
+	flip=$(($RANDOM%2))
+	if  [[ $flip -eq 0 ]]
+	then
+		singlet[$i]=0
+		((singleHead++))
+	else
+		singlet[$i]=1
+		((singleTail++))
+	fi
+done
+
+echo "percentage of heads for singlet dictionary:"$singleHead%
+echo "percentage of tails for singlet dictionary:"$singleTail%
